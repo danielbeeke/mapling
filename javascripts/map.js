91d6cf19d9c651ab8ab2e52275540e348ff4a5fe
root <root@aegir.local>
2013-02-27 02:39:59 +0100

    different map.js

--- javascripts/map.js
+++ javascripts/map.js
@@ -7,10 +7,29 @@
         var map = L.map('map', {
             attributionControl: false,
             zoomControl: false
-        }).setView([51.505, -0.09], 13);
+        });
 
         // add an OpenStreetMap tile layer
         L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-vyofok3q/{z}/{x}/{y}.png').addTo(map);
+
+        map.locate({setView: true, maxZoom: 16});
+
+        function onLocationFound(e) {
+            var radius = e.accuracy / 2;
+
+            L.marker(e.latlng).addTo(map)
+                .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+            L.circle(e.latlng, radius).addTo(map);
+        }
+
+        map.on('locationfound', onLocationFound);
+
+        function onLocationError(e) {
+            alert(e.message);
+        }
+
+        map.on('locationerror', onLocationError);
 
     }
   };
